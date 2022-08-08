899c86f7386e02937bc40ae404a4e68c2c8b175f
grazomarin <huseynovk355@gmail.com>
2022-08-08 17:05:05 +0400

    fixed exponential duplication of books, added working read status button

--- script.js
+++ script.js
@@ -4,23 +4,26 @@ const modal = document.getElementById('modal')
 const closeModalButton = document.querySelector('[data-close-button]')
 const overlay = document.getElementById('overlay')
 const submitBtn = document.querySelector('.submit')
+const form = document.getElementById('form')
 
 let myLibrary = []
 
 addBook.addEventListener('click', () => {
     openModal()
-    submitBtn.addEventListener('click', () => {
-        let name = document.getElementById('name').value
-        let author = document.getElementById('author').value
-        let pages = document.getElementById('pages').value
-        let read = document.getElementById('read').value
-        let book = new Book(name, author, pages, read)
-        myLibrary.push(book)
-        addBookToLibrary(name, author, pages, read, book);
-        closeModal(modal)
-        clearForm()
-    })
 }) 
+
+submitBtn.addEventListener('click', () => {
+    let name = document.getElementById('name').value
+    let author = document.getElementById('author').value
+    let pages = document.getElementById('pages').value
+    let read = document.getElementById('read').checked
+    console.log(read)
+    let book = new Book(name, author, pages, read)
+    myLibrary.push(book)
+    addBookToLibrary(name, author, pages, read, book);
+    closeModal(modal)
+})
+
 
 closeModalButton.addEventListener('click', () => {
     closeModal(modal);
@@ -44,19 +47,43 @@ function addBookToLibrary (name, author, pages, read, book) {
     const bookAuthor = containerDiv.querySelector(':nth-child(2)')
     const bookPages = containerDiv.querySelector(':nth-child(3)')
     const btnRmv = containerDiv.querySelector('.remove')
-    const btnStatus = containerDiv.querySelector('.readStatus')
+    let btnStatus = containerDiv.querySelector('.readStatus')
 
     bookName.textContent = `Book Name: ${name}`
     bookAuthor.textContent = `Author Name: ${author}`
     bookPages.textContent = `Pages: ${pages}`
     btnRmv.textContent = 'Remove'
-    btnStatus.textContent = 'Read'
+
+    assignRead(read, btnStatus)
+    btnStatus.addEventListener('click', () => {
+        if (read === true) {
+            btnStatus.classList.toggle('notRead');
+            btnStatus.classList.toggle('read');
+            btnStatus.textContent = 'Not Read'
+            read = false
+        } else {
+            btnStatus.classList.toggle('read');
+            btnStatus.classList.toggle('notRead');
+            btnStatus.textContent = 'Read';
+            read = true
+        }
+    });
 
     btnRmv.addEventListener('click', () => {
         containerDiv.remove()
         let index = myLibrary.indexOf(book)
         myLibrary.splice(index, 1)
     })
+}
+
+function assignRead(read, btn) {
+    if (read === true) {
+        btn.classList.toggle('read');
+        btn.textContent = 'Read'
+    } else {
+        btn.classList.toggle('notRead');
+        btn.textContent = 'Not Read';
+    }
 }
 
 function generateBookTemplate() {
@@ -86,14 +113,5 @@ function closeModal (modal) {
     overlay.classList.remove('active')
 }
 
-function clearForm(name, author, pages, read) {
-    name.value = undefined;
-    author.value = undefined;
-    pages.value = undefined;
-    read.value = off;
-}
-
 
 //fix the wrapping issue when text is too long
-// make read status work
-// the array is for future implementation of the database
